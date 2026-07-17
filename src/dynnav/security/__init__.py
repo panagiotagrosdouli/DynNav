50c28b8276833e72411a73026994086f14b73c43
Panagiota Grosdouli <75089541+panagiotagrosdouli@users.noreply.github.com>
2026-07-17 10:46:37 +0300

    Export fusion attribution mitigation and state APIs

--- src/dynnav/security/__init__.py
+++ src/dynnav/security/__init__.py
@@ -1,24 +1,48 @@
 """Cyber-physical security research primitives for DynNav."""
 
+from .attribution import AttributionEvidence, attribute_anomaly
 from .detectors import CUSUMDetector, DetectorConfig, EWMADetector, NISDetector, create_detector
 from .events import AttributionResult, DetectorResult, ResidualEvent, TrustState
+from .fusion import FusedTrust, FusionMethod, fuse_trust
+from .mitigation import (
+    MitigationAction,
+    MitigationDecision,
+    NavigationContext,
+    choose_mitigation,
+    security_risk,
+)
 from .statistics import chi_square_quantile, chi_square_quantile_fallback, mahalanobis_squared, validate_covariance
+from .temporal import SecurityState, SecurityStateMachine, StateMachineConfig, StateTransition
 from .trust import SourceTrustEstimator, TrustConfig
 
 __all__ = [
+    "AttributionEvidence",
     "AttributionResult",
     "CUSUMDetector",
     "DetectorConfig",
     "DetectorResult",
     "EWMADetector",
+    "FusedTrust",
+    "FusionMethod",
+    "MitigationAction",
+    "MitigationDecision",
     "NISDetector",
+    "NavigationContext",
     "ResidualEvent",
+    "SecurityState",
+    "SecurityStateMachine",
     "SourceTrustEstimator",
+    "StateMachineConfig",
+    "StateTransition",
     "TrustConfig",
     "TrustState",
+    "attribute_anomaly",
     "chi_square_quantile",
     "chi_square_quantile_fallback",
+    "choose_mitigation",
     "create_detector",
+    "fuse_trust",
     "mahalanobis_squared",
+    "security_risk",
     "validate_covariance",
 ]
